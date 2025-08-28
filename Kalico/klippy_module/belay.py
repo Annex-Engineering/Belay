f13c8585aa18eb7449cc519123125ad5be1fc399
Ryan Ghosh <rghosh776@gmail.com>
2025-08-28 01:22:31 -0700

    First pass at analog sensor support

--- Kalico/klippy_module/belay.py
+++ Kalico/klippy_module/belay.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023-2025 Ryan Ghosh <rghosh776@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+import math
 from abc import ABC, abstractmethod
 
 
@@ -15,12 +16,12 @@ class Belay:
         self.name = config.get_name().split()[1]
 
         # create slider sensor
-        self.slider_sensor = self._get_selected_subclass(
+        self.slider_sensor = get_selected_subclass(
             SliderSensor, "sensor_type", config
         )(config, self)
 
         # create secondary extruder
-        self.secondary_extruder = self._get_selected_subclass(
+        self.secondary_extruder = get_selected_subclass(
             SecondaryExtruder, "extruder_type", config
         )(config, self)
 
@@ -49,6 +50,7 @@ class Belay:
         # other variables
         self.enabled = False
         self.user_disable = False
+        self.next_cmd_generator = None
         self.gcode = self.printer.lookup_object("gcode")
 
         # register commands
@@ -80,12 +82,13 @@ class Belay:
             self.cmd_BELAY_CLEAR_OVERRIDE,
             desc=self.cmd_BELAY_CLEAR_OVERRIDE_help,
         )
-
-    def _get_selected_subclass(self, parent_class, config_option, config):
-        choices = {}
-        for cls in parent_class.__subclasses__():
-            choices[cls.get_name()] = cls
-        return config.getchoice(config_option, choices)
+        self.gcode.register_mux_command(
+            "BELAY_NEXT",
+            "BELAY",
+            self.name,
+            self.cmd_BELAY_NEXT,
+            desc=self.cmd_BELAY_NEXT_help,
+        )
 
     def enable(self):
         if self.enabled or self.user_disable:
@@ -120,6 +123,10 @@ class Belay:
         self.set_multiplier(1.0, print_msg=False)
         if self.debug_level >= 1:
             self.gcode.respond_info("Reset secondary extruder multiplier")
+
+    def start_next_cmd_generator(self, generator):
+        self.next_cmd_generator = generator
+        next(self.next_cmd_generator)
 
     cmd_QUERY_BELAY_help = "Report Belay sensor state"
 
@@ -158,6 +165,22 @@ class Belay:
 
     def cmd_BELAY_CLEAR_OVERRIDE(self, gcmd):
         self.user_disable = False
+
+    cmd_BELAY_NEXT_help = (
+        "You will be prompted to use this command if Belay requires user"
+        " confirmation"
+    )
+
+    def cmd_BELAY_NEXT(self, gcmd):
+        if self.next_cmd_generator:
+            try:
+                next(self.next_cmd_generator)
+            except Exception as e:
+                self.next_cmd_generator = None
+                if not isinstance(e, StopIteration):
+                    raise
+        else:
+            raise self.printer.command_error("BELAY_NEXT command is inactive")
 
     def get_status(self, eventtime):
         status = {
@@ -252,6 +275,21 @@ class NamedConfigOptionChoice(ABC):
     @abstractmethod
     def get_name(cls):
         pass
+
+
+def get_selected_subclass(parent_class, config_option, config, default=None):
+    choices = {}
+    for cls in parent_class.__subclasses__():
+        choices[cls.get_name()] = cls
+    if default is None:
+        return config.getchoice(config_option, choices)
+    if default not in choices:
+        raise Exception(
+            "Default '{}' is not among subclass names of parent class {}".format(
+                default, parent_class
+            )
+        )
+    return config.getchoice(config_option, choices, default=default)
 
 
 # Slider sensors
@@ -490,6 +528,299 @@ class DualDigitalSwitch(SliderSensor):
             self.multiplier_mid,
             minval=self.multiplier_low,
             maxval=self.multiplier_high,
+        )
+
+
+class AnalogSliderSensor(SliderSensor):
+    def __init__(self, config, belay):
+        self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
+        self.belay = belay
+
+        # read slider travel
+        self.slider_travel = config.getfloat("slider_travel", above=0.0)
+        self.slider_travel_half = self.slider_travel / 2
+
+        # create sensor (for converting ADC readings to slider positions)
+        self.sensor = get_selected_subclass(
+            AnalogPositionSensor, "sensor_subtype", config
+        )(config, -self.slider_travel_half, self.slider_travel_half)
+
+        # set up ADC callback
+        ppins = self.printer.lookup_object("pins")
+        self.mcu_adc = ppins.setup_pin("adc", config.get("sensor_pin"))
+        adc_sample_time = config.getfloat(
+            "adc_sample_time", default=0.001, above=0.0
+        )
+        adc_sample_count = config.getint(
+            "adc_sample_count", default=1, minval=1
+        )
+        control_loop_interval = config.getfloat(
+            "control_loop_interval", default=0.1, above=0.0
+        )
+        self.mcu_adc.setup_minmax(adc_sample_time, adc_sample_count)
+        self.mcu_adc.setup_adc_callback(
+            control_loop_interval, self.adc_callback
+        )
+
+        # create controller
+        slider_setpoint = config.getfloat(
+            "slider_setpoint",
+            default=0.0,
+            above=-self.slider_travel_half,
+            below=self.slider_travel_half,
+        )
+        multiplier_max_offset = config.getfloat(
+            "multiplier_max_offset", default=0.5, above=0.0, below=1.0
+        )
+        self.controller = SliderPIDController(
+            config, slider_setpoint, multiplier_max_offset
+        )
+
+        # read other values
+        msg_interval = config.getint(
+            "message_interval",
+            default=5,
+            above=math.ceil(control_loop_interval),
+        )
+        self.loops_per_msg = round(msg_interval / control_loop_interval)
+
+        # other variables
+        self.toolhead = None
+        self.last_slider_pos = 0.0
+        self.last_raw_multiplier_offset = 0.0
+        self.msg_loop_counter = 0
+        self.gcode = self.printer.lookup_object("gcode")
+
+        # register_commands
+        self.gcode.register_mux_command(
+            "BELAY_CALIBRATE",
+            "BELAY",
+            self.belay.name,
+            self.cmd_BELAY_CALIBRATE,
+            desc=self.cmd_BELAY_CALIBRATE_help,
+        )
+        self.gcode.register_mux_command(
+            "BELAY_SET_SLIDER_SETPOINT",
+            "BELAY",
+            self.belay.name,
+            self.cmd_BELAY_SET_SLIDER_SETPOINT,
+            desc=self.cmd_BELAY_SET_SLIDER_SETPOINT_help,
+        )
+
+    def handle_connect(self):
+        self.toolhead = self.printer.lookup_object("toolhead")
+
+    @classmethod
+    def get_name(cls):
+        return "analog_position_sensor"
+
+    def handle_extrusion_direction_update(self, new_direction):
+        self.last_direction = new_direction
+        if self.belay.enabled:
+            self._set_multiplier(self.last_raw_multiplier_offset)
+
+    def handle_enable(self):
+        eventtime = self.reactor.monotonic()
+        mcu = self.printer.lookup_object("mcu")
+        est_print_time = mcu.estimated_print_time(eventtime)
+        extruder = self.toolhead.get_extruder()
+        last_e_pos = extruder.find_past_position(est_print_time)
+        self.controller.reset(self.last_slider_pos, last_e_pos)
+        self.last_raw_multiplier_offset = 0.0
+
+    def handle_disable(self):
+        return
+
+    def get_state_description(self):
+        slider_pos_percentage = round(
+            self.last_slider_pos / self.slider_travel_half * 100
+        )
+        if self.last_slider_pos >= 0.0:
+            return "compressed {}mm ({}%) from center".format(
+                self.last_slider_pos, slider_pos_percentage
+            )
+        return "expanded {}mm ({}%) from center".format(
+            -self.last_slider_pos, -slider_pos_percentage
+        )
+
+    def get_dimensionless_position(self):
+        return self.last_slider_pos / self.slider_travel_half
+
+    def adc_callback(self, read_time, read_value):
+        # note slider position
+        self.last_slider_pos = self.sensor.get_position(read_time, read_value)
+        if not self.belay.enabled:
+            return
+
+        # update extruder multiplier
+        extruder = self.toolhead.get_extruder()
+        e_pos = extruder.find_past_position(read_time)
+        self.last_raw_multiplier_offset = self.controller.update(
+            self.last_slider_pos, e_pos
+        )
+        self._set_multiplier(
+            self.last_raw_multiplier_offset,
+            print_msg=(self.msg_loop_counter == 0),
+        )
+        self.msg_loop_counter = (self.msg_loop_counter + 1) % self.loops_per_msg
+
+    def _set_multiplier(self, raw_multiplier_offset, print_msg=False):
+        if self.last_direction:
+            multiplier_offset = -raw_multiplier_offset
+        else:
+            multiplier_offset = raw_multiplier_offset
+        self.belay.set_multiplier(1.0 + multiplier_offset, print_msg=print_msg)
+
+    cmd_BELAY_CALIBRATE_help = "Calibrate the Belay sensor"
+
+    def cmd_BELAY_CALIBRATE(self, gcmd):
+        self.belay.start_next_cmd_generator(self._calibrate())
+
+    def _calibrate(self):
+        self.gcode.respond_info(
+            "Move belay {} to its fully expanded state, and hold it there while"
+            " using BELAY_NEXT to continue calibration.".format(self.belay.name)
+        )
+        yield
+
+        min_pos_reading = self.mcu_adc.get_last_value()[0]
+        self.gcode.respond_info(
+            "Move belay {} to its fully compressed state, and hold it there"
+            " while using BELAY_NEXT to continue calibration.".format(
+                self.belay.name
+            )
+        )
+        yield
+
+        max_pos_reading = self.mcu_adc.get_last_value()[0]
+        sensor_msg = self.sensor.update_calibration(
+            min_pos_reading, max_pos_reading
+        )
+        self.gcode.respond_info(
+            "Calibration complete:\n{}\nMake sure to update the printer config"
+            " file with these parameters so they will be kept across restarts:".format(
+                sensor_msg
+            )
+        )
+
+    cmd_BELAY_SET_SLIDER_SETPOINT_help = "Set the target slider position"
+
+    def cmd_BELAY_SET_SLIDER_SETPOINT(self, gcmd):
+        if gcmd.get_int("DIMENSIONLESS", 0):
+            setpoint = (
+                gcmd.get_float("SETPOINT", default=0.0, above=-1.0, below=1.0)
+                * self.slider_travel_half
+            )
+        else:
+            setpoint = gcmd.get_float(
+                "SETPOINT",
+                default=0.0,
+                above=-self.slider_travel_half,
+                below=self.slider_travel_half,
+            )
+        self.controller.set_slider_setpoint(setpoint)
+
+    def get_status(self, eventtime):
+        return {"slider_position": self.last_slider_pos}
+
+
+class SliderPIDController:
+    def __init__(self, config, setpoint_initial, output_min_max_abs):
+        self.slider_setpoint = setpoint_initial
+        self.output_min_max_abs = output_min_max_abs
+
+        # read values from config
+        self.Kp = config.getfloat("pid_Kp", minval=0.0)  # proportional gain
+        self.Ki = config.getfloat("pid_Ki", above=0.0)  # integral gain
+        self.Kd = config.getfloat("pid_Kd", minval=0.0)  # derivative gain
+        self.Ti = self.Kp / self.Ki  # integral "time"
+        self.Td = self.Kd / self.Kp  # derivative "time"
+        self.Tt = config.getfloat(  # tracking "time" constant
+            "pid_Tt",
+            default=math.sqrt(self.Ti * self.Td),
+            above=self.Td,
+            below=self.Ti,
+        )
+
+        # other variables
+        self.last_slider_pos = 0.0
+        self.last_e_pos = 0.0
+        self.last_error = 0.0
+        self.integral = 0.0
+
+    def update(self, slider_pos, e_pos):
+        error = self.slider_setpoint - slider_pos
+        de = abs(e_pos - self.last_e_pos)
+
+        proportional = self.Kp * error
+        self.integral += self.Ki * (self.last_error + error) / 2.0 * de
+        derivative = -self.Kd * (slider_pos - self.last_slider_pos) / de
+        output_raw = proportional + self.integral + derivative
+        output_clamped = min(
+            max(output_raw, -self.output_min_max_abs),
+            self.output_min_max_abs,
+        )
+
+        self.integral += (output_clamped - output_raw) * de / max(self.Tt, de)
+        self.last_slider_pos = slider_pos
+        self.last_e_pos = e_pos
+        self.last_error = error
+
+        return output_clamped
+
+    def set_slider_setpoint(self, setpoint):
+        self.slider_setpoint = setpoint
+
+    def reset(self, last_slider_pos, last_e_pos):
+        self.last_slider_pos = last_slider_pos
+        self.last_e_pos = last_e_pos
+        self.last_error = self.slider_setpoint - self.last_slider_pos
+        self.integral = 0.0
+
+
+class AnalogPositionSensor(NamedConfigOptionChoice, ABC):
+    @abstractmethod
+    def __init__(self, config, min_position, max_position):
+        pass
+
+    @abstractmethod
+    def get_position(self, read_time, read_value):
+        pass
+
+    @abstractmethod
+    def update_calibration(self, min_position_reading, max_position_reading):
+        pass
+
+
+class LinearPotentiometer(AnalogPositionSensor):
+    def __init__(self, config, min_position, max_position):
+        self.min_pos = min_position
+        self.max_pos = max_position
+        self.pos_span = self.max_pos - self.min_pos
+        self.min_pos_reading = config.get_float(
+            "min_position_reading", default=0.0
+        )
+        self.max_pos_reading = config.get_float(
+            "max_position_reading", default=1.0
+        )
+
+    @classmethod
+    def get_name(cls):
+        return "linear_potentiometer"
+
+    def get_position(self, read_time, read_value):
+        reading_span = self.max_pos_reading - self.min_pos_reading
+        return (
+            self.min_pos
+            + (read_value - self.min_pos_reading) / reading_span * self.pos_span
+        )
+
+    def update_calibration(self, min_position_reading, max_position_reading):
+        self.min_pos_reading = min_position_reading
+        self.max_pos_reading = max_position_reading
+        return "min_position_reading: {}\nmax_position_reading: {}".format(
+            min_position_reading, max_position_reading
         )
 
 
